028959ef8d1ddaf746a8a1573c17eef28ba2bb68
pritikanaik1024 <121875645+pritikanaik1024@users.noreply.github.com>
2024-07-26 12:12:43 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# GovJobnet
-An application that leverages government-provided employment data to send job notifications to candidates based on their geospatial coordinates.
--- govjobnet/firebase.js
+++ govjobnet/firebase.js
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
   apiKey: "AIzaSyBRRdA0IXvFOehu01migpso5h3QglJEhew",
   authDomain: "govjobnet.firebaseapp.com",
@@ -12,12 +9,6 @@ const firebaseConfig = {
   appId: "1:883577938123:web:239eeadade30a7df28fb4e",
   measurementId: "G-EGP854ZVRM"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// src/firebase.js
-import firebase from 'firebase/app';
-import 'firebase/auth';
-firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
+export default firebaseApp;
