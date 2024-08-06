8894030d41bfe8a9e3b2bf96c9179dcb14a47744
pritikanaik1024 <121875645+pritikanaik1024@users.noreply.github.com>
2024-08-07 00:12:25 +0530

    Updates

--- government-job-search/src/firebaseConfig.js
+++ government-job-search/src/firebaseConfig.js
@@ -1,5 +1,11 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBRRdA0IXvFOehu01migpso5h3QglJEhew",
   authDomain: "govjobnet.firebaseapp.com",
@@ -9,6 +15,7 @@ const firebaseConfig = {
   appId: "1:883577938123:web:239eeadade30a7df28fb4e",
   measurementId: "G-EGP854ZVRM"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export default firebaseApp;
